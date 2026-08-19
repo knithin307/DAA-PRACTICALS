a693cf4b01b9f9c8d4a3a8c121f4e4de7aa94445
K NITHIN <kotha.nithin132981@marwadiuniversity.ac.in>
2026-08-20 00:30:27 +0530

    Add files via upload

---
+++ PRACTICAL 2/LINEAR SEARCH.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+int arr[]={10,30,40,20,50};
+int n =5;
+int num = 40;
+for(int i=0;i<n;i++){
+    if (arr[i]==num){
+        printf("number is found");
+            return 0;
+            
+            }
+    }
+        printf("not found");
+}
